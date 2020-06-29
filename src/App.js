f473063defc5c986233d74c500e0b2a20ee6929c
Éric <errhud@gmail.com>
2020-06-29 11:52:43 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Contact from "./components/Contact";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Contact Manager</h1>
+        </header>
+
+        <Contact />
+      </div>
+    );
+  }
+}
+
+export default App;
